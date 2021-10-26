643a8449580c385ac57d2fbc608846761ae437be
DawonEllaKim <dawonella0411@gmail.com>
2021-10-26 22:53:34 +0900

    [feat] 메인페이지 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route } from "react-router";
 
 import { BrowserRouter } from "react-router-dom";
+import Main from "../src/pages/Main";
 import LogIn from "../src/pages/LogIn";
 import SignUp from "../src/pages/SignUp";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Route exact path="/" component={Main} />
         <Route exact path="/logIn" component={LogIn} />
         <Route exact path="/signUp" component={SignUp} />
       </BrowserRouter>
