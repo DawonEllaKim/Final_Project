97fead88fde017fc079c21ac6c55d766a023338f
DawonEllaKim <dawonella0411@gmail.com>
2021-11-02 18:15:42 +0900

    [fix] 메인 뷰 수정

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,133 +2,87 @@ import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 
+// React Icons
+import { BsGenderMale } from "react-icons/bs";
+import { BsGenderFemale } from "react-icons/bs";
+
 const Card = (props) => {
   const postList = useSelector((state) => state.post.list[props.index]);
-  const dogSize = postList.dogSize;
+  // const dogSize = postList.dogSize;
   const dogGender = postList.dogGender;
   const dogAge = postList.dogAge;
-  const locationCategory = postList.locationCategory;
+  // const locationCategory = postList.locationCategory;
   const dogImage = postList.dogImage;
   const dogName = postList.dogName;
   const meetingTime = postList.meetingTime;
   const meetingDate = postList.meetingDate;
-  const completed = postList.completed;
+  // const completed = postList.completed;
+  const wishList = postList.wishList;
 
   return (
     <Wrap>
-      <Top>
-        <Left>
-          <Image src={dogImage} />
-        </Left>
-        <Right>
-          <Title>
-            <div>이름</div>
-            <div>정보</div>
-            <div>위치</div>
-          </Title>
-          <Info>
-            <div>{dogName}</div>
-            <GenderAge>
-              <div style={{ marginRight: "30px" }}>{dogSize}</div>
-              <div style={{ marginRight: "30px" }}>{dogGender}</div>
-              <div>{dogAge}</div>
-            </GenderAge>
-            <div>{locationCategory}</div>
-          </Info>
-        </Right>
-      </Top>
-      <Bottom>
-        <div style={{ marginRight: "30px" }}>산책시간</div>
-        <div style={{ marginRight: "30px" }}>
-          {meetingDate + " " + meetingTime}
-        </div>
-      </Bottom>
-      <Complete>{completed ? "마감" : "진행중"}</Complete>
+      <img src={dogImage} />
+      <Info>
+        <Top>
+          <h4> {dogGender === "남" ? <BsGenderMale /> : <BsGenderFemale />}</h4>
+          <p>{dogName + ", " + dogAge}</p>
+        </Top>
+        <Center>{wishList}</Center>
+        <Bottom>{meetingDate + " " + meetingTime + " >"}</Bottom>
+      </Info>
     </Wrap>
   );
 };
 const Wrap = styled.div`
-  position: relative;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
-
   width: 350px;
-  height: 152px;
+  height: 176px;
   margin-bottom: 12px;
-  border-radius: 14px;
-  background-color: #e5e5e5;
-
+  padding: 12px;
+  border-radius: 25px;
+  background-color: #ebebeb;
   font-size: 14px;
   font-weight: 400;
   line-height: 20.27px;
-
   cursor: pointer;
+  img {
+    width: 152px;
+    height: 152px;
+    border-radius: 25px;
+  }
+`;
+const Info = styled.div`
+  width: 192px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding-left: 20px;
 `;
 const Top = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  h4 {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+  }
+  p {
+    font-size: 16px;
+  }
 `;
-const Left = styled.div``;
-const Right = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 242px;
-  padding-top: 20px;
-`;
-const GenderAge = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`;
-const Title = styled.div`
-  width: 50px;
-  border-right: 1px solid white;
-`;
-const Info = styled.div`
-  width: 192px;
-`;
-const Bottom = styled.div`
-  position: absolute;
-  bottom: 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+const Center = styled.div`
   width: 100%;
   padding: 10px;
-
-  border-top: 1px solid white;
+`;
+const Bottom = styled.div`
+  width: 100%;
+  padding: 10px;
 `;
 
-const Image = styled.img`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 108px;
-  height: 108px;
-  border-radius: 14px 14px 0 0;
-`;
-const Complete = styled.div`
-  position: absolute;
-  top: 5px;
-  right: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 12%;
-  height: 25px;
-  background-color: pink;
-  border-radius: 20px;
-  font-size: 12px;
-`;
 export default Card;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,30 @@
 import React, { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import Slider from "react-slick";
 
+// Redux
 import { history } from "../redux/configureStore";
 import { actionCreators as postActions } from "../redux/modules/post";
 
+// Components
 import Card from "../components/Card";
 import DogSize from "../components/MainSideBar/Filters/DogSize";
 import DogGender from "../components/MainSideBar/Filters/DogGender";
 import DogAge from "../components/MainSideBar/Filters/DogAge";
 import LocationCategory from "../components/MainSideBar/Filters/LocationCategory";
+
+// React Icons
+import { AiOutlineFilter } from "react-icons/ai";
+import { GrNotification } from "react-icons/gr";
+import { AiOutlineHome } from "react-icons/ai";
+import { BsChatRightDots } from "react-icons/bs";
+import { BsPerson } from "react-icons/bs";
+import { MdLocationOn } from "react-icons/md";
+
+// 슬라이드
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Main = () => {
   const dispatch = useDispatch();
@@ -34,37 +49,72 @@ const Main = () => {
     dispatch(postActions.getPostMD());
   }, []);
 
+  // 슬라이드 세팅
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: true,
+  };
+
   return (
     <Wrap ref={sideBarRef} onClick={closeSideBar}>
-      {/* 메세지 + 날씨 + 로그인하기 버튼 */}
+      {/* 필터 + 산책할개 + 알람 */}
       <Head>
-        <h1>보호자님,</h1>
-        <p>반려견을 등록해주세요</p>
-        <button>로그인하러가기</button>
-      </Head>
-      {/* 게시물 필터 모음 + 사이드 바*/}
-      <SubHead>
+        <div onClick={showSideBar}>
+          <AiOutlineFilter />
+        </div>
+        <p>산책할개</p>
         <div>
-          {/* Menu */}
-          <HamburgerNav onClick={showSideBar}>menu</HamburgerNav>
-          {/* 햄버거 메뉴 누르면 열리는 사이드 바 */}
-          <SideBarNav sideBar={sideBar}>
-            <Filter onClick={showSideBar}>나가기</Filter>
+          <GrNotification />
+        </div>
+      </Head>
+
+      {/* 일러스트 슬라이드 */}
+      <Container>
+        <StyledSlider {...settings}>
+          <div>
+            <Img src="https://images.unsplash.com/photo-1522276498395-f4f68f7f8454?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80" />
+          </div>
+          <div>
+            <Img src="https://images.unsplash.com/photo-1544567708-827a79119a78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80" />
+          </div>
+          <div>
+            <Img src="https://images.unsplash.com/photo-1560743173-567a3b5658b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" />
+          </div>
+        </StyledSlider>
+      </Container>
+
+      {/* 사이드 바*/}
+      <div>
+        {/* 햄버거 메뉴 누르면 열리는 사이드 바 */}
+        <SideBarNav sideBar={sideBar}>
+          <BarWrap>
+            <Filter onClick={showSideBar}>
+              <AiOutlineFilter
+                style={{ width: "23px", height: "23px", marginRight: "10px" }}
+              />
+              <p>Filter</p>
+              <button>초기화</button>
+            </Filter>
+
             <SubMenuWrap>
               <DogSize />
               <DogGender />
               <DogAge />
               <LocationCategory />
             </SubMenuWrap>
-          </SideBarNav>
-        </div>
-        <p>크기</p>
-        <p>성별</p>
-        <p>위치</p>
-        <p>마감여부</p>
-      </SubHead>
+          </BarWrap>
+        </SideBarNav>
+      </div>
+
       {/* 각 게시물에 대한 카드들 */}
       <Body>
+        <Text>같이 산책하실래요?</Text>
         {postList.map((post, index) => {
           return (
             <div onClick={() => history.push(`/posts/${post.id}`)}>
@@ -73,102 +123,84 @@ const Main = () => {
           );
         })}
       </Body>
+
       {/* 고정 버튼들 */}
       <Footer>
         <FooterLeft>
-          <Button onClick={() => history.push("/login")}>로그아웃</Button>
-          <Button>채팅방</Button>
-          <Button onClick={() => history.push("/mypage")}>마이페이지</Button>
+          <Button onClick={() => history.push("/")}>
+            <AiOutlineHome style={{ width: "20px", height: "20px" }} />
+          </Button>
+          <Button>
+            <BsChatRightDots style={{ width: "20px", height: "20px" }} />
+          </Button>
+          <Button onClick={() => history.push("/mypage")}>
+            <BsPerson style={{ width: "20px", height: "20px" }} />
+          </Button>
         </FooterLeft>
 
         <FooterRight>
-          <Button onClick={() => history.push("/write")}>산책 등록</Button>
+          <Button onClick={() => history.push("/write")}>
+            <MdLocationOn style={{ width: "30px", height: "50px" }} />
+          </Button>
         </FooterRight>
       </Footer>
     </Wrap>
   );
 };
 
+const Container = styled.div``;
+const StyledSlider = styled(Slider)`
+  .slick-slide div {
+    outline: none;
+  }
+  width: 350px;
+  height: 220px;
+  margin-bottom: 12px;
+  border-radius: 25px;
+`;
+const Img = styled.img`
+  width: 350px;
+  height: 220px;
+  background-size: cover;
+  border-radius: 25px;
+`;
 const Wrap = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 390px;
+  margin: auto;
 `;
-// 메세지 + 날씨 + 로그인하기 버튼
 const Head = styled.div`
-  box-sizing: border-box;
-  position: relative;
-  top: 0;
-  /* z-index: 2; */
-  display: flex;
-  flex-direction: column;
-  justify-content: left;
-  align-items: flex-start;
-  width: 100%;
-  height: 264px;
-  padding: 5%;
-  background-color: pink;
-  h1 {
-    margin-bottom: 10px;
-    font-size: 24px;
-    font-weight: 400;
-  }
-  p {
-    margin-bottom: 45px;
-  }
-  button {
-    width: 186px;
-    height: 40px;
-    margin: auto auto 15% auto;
-    border-radius: 20px;
-    border: none;
-  }
-`;
-// 게시물 필터 모음 + 사이드 바
-const SubHead = styled.div`
-  position: relative;
-  margin-bottom: 12px;
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
-  height: 76px;
-  background-color: #efefef;
-  border-radius: 14px;
-  /* z-index: 9; */
+  margin: 30px auto 24px auto;
+  padding: 0 34px;
+  div {
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+  }
   p {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 15%;
-    height: 32px;
-    font-size: 14px;
-    border-radius: 20px;
-    background-color: #c4c4c4;
+    width: 150px;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 35px;
+    color: #393939;
+    cursor: pointer;
   }
 `;
-const HamburgerNav = styled.div`
-  /* position: fixed;
-  top: 0;
-  left: 0; */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 44px;
-  height: 44px;
-  /* margin: 50px 24px; */
-  background: #c4c4c4;
-  border-radius: 50%;
-  color: purple;
-  text-decoration: none;
-  cursor: pointer;
-  &:hover {
-    color: red;
-    transition: 350ms;
-  }
+const Slide = styled.div`
+  width: 350px;
+  height: 220px;
+  margin-bottom: 12px;
+  border-radius: 25px;
+  background-color: #c4c4c4;
 `;
 const SideBarNav = styled.div`
   position: fixed;
@@ -177,13 +209,19 @@ const SideBarNav = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  width: 65%;
+  width: 100vw;
   height: 100vh;
-  margin-top: 44px;
-  border-radius: 0 20px 20px 0;
-  background: #c4c4c4;
+  background: rgba(0, 0, 0, 0.8);
   transition: 350ms;
   z-index: 10;
+`;
+const BarWrap = styled.div`
+  background-color: #c4c4c4;
+  width: 308px;
+  height: 100%;
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
+  margin-top: 44px;
 `;
 const Filter = styled.div`
   box-sizing: border-box;
@@ -194,25 +232,34 @@ const Filter = styled.div`
   padding-left: 26.26px;
   font-size: 18px;
   font-weight: 400;
-  color: purple;
   text-decoration: none;
   cursor: pointer;
-  &:hover {
-    color: red;
-    transition: 350ms;
+  p {
+    font-size: 18px;
+    margin-right: 130px;
+  }
+  button {
+    border: none;
+    background-color: transparent;
+    font-size: 18px;
   }
 `;
 const SubMenuWrap = styled.div`
   width: 100%;
 `;
-// 각 게시물에 대한 카드들
 const Body = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
-// 고정 버튼들
+const Text = styled.p`
+  width: 152px;
+  height: 16px;
+  margin: 32px 218px 24px 0;
+  font-size: 16px;
+  font-weight: 700;
+`;
 const Footer = styled.div`
   display: flex;
   flex-direction: row;
@@ -229,12 +276,13 @@ const FooterLeft = styled.div`
   width: 270px;
   height: 60px;
   margin: 12px;
+  padding: 20px;
   border-radius: 20px;
 
   width: 274px;
   height: 60px;
 
-  background-color: pink;
+  background-color: #5c5c5c;
   border-radius: 20px;
 `;
 const FooterRight = styled.div`
@@ -243,7 +291,7 @@ const FooterRight = styled.div`
   align-items: center;
   width: 60px;
   height: 60px;
-  background-color: pink;
+  background-color: #5c5c5c;
   border-radius: 50%;
 `;
 const Button = styled.button`
