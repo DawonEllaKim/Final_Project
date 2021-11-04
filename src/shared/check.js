bccfd8f4048d5cd06aa00842e44b94619d3f1926
hyojin-k <hyo.khj@gmail.com>
2021-11-04 12:12:16 +0900

    [feat] 견종 유효성검사 추가

--- src/shared/check.js
+++ src/shared/check.js
@@ -5,7 +5,13 @@ export const emailCheck = (email) => {
   }
 
 //  패스워드 검사
-  export const passwordCheck = (password) => {
-    var regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}$/; //  8자 이상 영대소문자, 숫자 조합
-    return regExp.test(password);
-  }
+export const passwordCheck = (password) => {
+  var regExp = /^[0-9a-zA-Z]{8,20}$/; //  8자 이상 영대소문자, 숫자 조합
+  return regExp.test(password);
+}
+
+// 견종 문자만 가능하게 제한
+export const dogBreedCheck = (dogBreed) =>{
+  var regExp = /^[가-힣a-zA-Z]$/;
+  return regExp.test(dogBreed);
+} 
