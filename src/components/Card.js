41382c97a1b2ff12a6c4762c8e38bb76b5f92261
hyojin-k <hyo.khj@gmail.com>
2021-11-13 13:42:17 +0900

    [style] 카드 수정

--- src/components/Card.js
+++ src/components/Card.js
@@ -39,7 +39,7 @@ const Card = ({ post }) => {
             {dogGender === "남" ? (
               <img src={male} style={{ width: "16px", height: "16px", borderRadius:'0' }} />
             ) : (
-              <img src={female} style={{ width: "16px", height: "16px", borderRadius:'0' }} />
+              <img src={female} style={{ width: "16px", height: "16px", borderRadius:'0'}} />
             )}
           </h4>
           <p>{dogName + ", " + dogAge}</p>
@@ -47,7 +47,7 @@ const Card = ({ post }) => {
         <CardCenter>
           <p>장소 : 서울숲</p>
           <p>크기 : {post.dogSize}</p>
-          <p>{dogComment}</p>
+          <p styoe={{wordBreak:'keep-all'}}>{dogComment}</p>
         </CardCenter>
         <CardBottom>
           {year}. {month}. {day} {hour}시 {minute}분
@@ -122,11 +122,12 @@ const CardCenter = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: flex-start;
+  text-align: left;
   font-size: 14px;
-  line-height: 1.7;
+  line-height: 1.4;
   width: 100%;
-  padding-top: 8px;
+  padding: 8px 8px 0 0;
+  box-sizing: border-box;
 `;
 const CardBottom = styled.div`
   position: absolute;
