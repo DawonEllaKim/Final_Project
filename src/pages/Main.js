31449ca629a65089ea1a5d884a4c9c0995bb744a
DawonEllaKim <dawonella0411@gmail.com>
2021-11-21 17:15:42 +0900

    [feat] 날씨 문구, 스타일 생성

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -141,7 +141,7 @@ const Main = (props) => {
           ) : (
             <StyledSlider {...topSettings} style={{ cursor: "pointer" }}>
               <Weather />
-              {/* <div
+              <div
                 onClick={() => {
                   history.push("/caution1");
                 }}
@@ -161,7 +161,7 @@ const Main = (props) => {
                 }}
               >
                 <Img src="https://images.unsplash.com/photo-1560743173-567a3b5658b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80" />
-              </div> */}
+              </div>
             </StyledSlider>
           )}
         </div>
