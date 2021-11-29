1c6bb7eaaaa3b5ca692e7e048ea9aaf5b4e9d2e2
hyojin-k <hyo.khj@gmail.com>
2021-11-29 10:20:14 +0900

    [style] 댓글 이미지 깨짐 수정

--- src/components/DogstaComment/CommentWrite.js
+++ src/components/DogstaComment/CommentWrite.js
@@ -73,7 +73,7 @@ const UserImage = styled.img`
 `;
 const CommentInput = styled.input`
   display: block;
-  width: 100%;
+  width: calc(100% - 60px);
   font-size: 16px;
   border: 1px solid #bdbdbd;
   box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
